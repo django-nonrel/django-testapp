acf2007e9765afa43f58b77b20aacb6b54933eea
Waldemar Kornewald <wkornewald>
2009-10-20 13:42:52 +0200

    Added unit tests for result ordering.

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,3 +1,4 @@
 from app.tests.field_db_convertion import FieldDBConvertionTest
 from app.tests.field_options import FieldOptionsTest
 #from app.tests.filter import FilterTest
+from app.tests.order import OrderTest
