3f14dcc587f81694313c3033d4ce78305930f194
Waldemar Kornewald <wkornewald>
2010-04-11 13:21:57 +0200

    added djangotoolbox to installed apps

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
     'djangoappengine',
+    'djangotoolbox',
 #    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
