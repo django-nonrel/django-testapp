f087a5ad6f78ab19d0e31c8c591921dbb9fe7ce5
Waldemar Kornewald <wkornewald>
2010-05-25 11:45:39 +0200

    added better base settings, so djangoappengine is used only if available (better for MongoDB users)

--- settings.py
+++ settings.py
@@ -1,16 +1,27 @@
-from djangoappengine.settings_base import *
+try:
+    from djangoappengine.settings_base import *
+    has_djangoappengine = True
+except ImportError:
+    has_djangoappengine = False
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+
+import os
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
-    'djangoappengine',
     'djangotoolbox',
 #    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
 )
 
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+if has_djangoappengine:
+    INSTALLED_APPS = ('djangoappengine',) + INSTALLED_APPS
 
-import os
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
+
+ROOT_URLCONF = 'urls'
