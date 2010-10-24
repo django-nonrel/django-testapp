ac363e09d38134f3e9efe7020e51d80be1fb674f
Waldemar Kornewald <wkornewald>
2010-10-24 11:32:33 +0200

    got rid of customized manage.py. Django's normal manage.py finally works unmodified

--- manage.py
+++ manage.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-
-# Add "common-apps" folder to sys.path if it exists
-import os, sys
-common_dir = os.path.join(os.path.dirname(__file__), 'common-apps')
-if os.path.exists(common_dir):
-    sys.path.append(common_dir)
-
-# Initialize App Engine SDK if djangoappengine backend is installed
-try:
-    from djangoappengine.boot import setup_env
-except ImportError:
-    pass
-else:
-    setup_env()
-
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
--- settings.py
+++ settings.py
@@ -1,32 +1,37 @@
-try:
-    from djangoappengine.settings_base import *
-    has_djangoappengine = True
-except ImportError:
-    has_djangoappengine = False
-    DEBUG = True
-    TEMPLATE_DEBUG = DEBUG
+# Initialize App Engine and import the default settings (DB backend, etc.).
+# If you want to use a different backend you have to remove all occurences
+# of "djangoappengine" from this file.
+from djangoappengine.settings_base import *
 
 import os
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
-    'djangotoolbox',
+    'djangoappengine',
+#    'django.contrib.admin',
 #    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'djangotoolbox',
 )
 
-if has_djangoappengine:
-    INSTALLED_APPS = ('djangoappengine',) + INSTALLED_APPS
-
+# This test runner captures stdout and associates tracebacks with their
+# corresponding output. Helps a lot with print-debugging.
 TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
 
 ADMIN_MEDIA_PREFIX = '/media/admin/'
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+STATICFILES_ROOT = os.path.join(os.path.dirname(__file__), '_generated_media')
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static'),
+)
+STATICFILES_URL = '/media/'
+MEDIA_URL = '/files/'
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 
 ROOT_URLCONF = 'urls'
+
+SITE_ID = 1
 
 # Activate django-dbindexer if available
 try:
