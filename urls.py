794fb481e36e168bcce77afca28befb574aef520
Waldemar Kornewald <wkornewald>
2010-10-28 15:02:14 +0200

    added a handler500 which uses a RequestContext

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
+handler500 = 'djangotoolbox.errorviews.server_error'
+
 urlpatterns = patterns('',
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
