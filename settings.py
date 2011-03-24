db0dd964cbd58efefc2a7056a84017cf0b30689e
Waldemar Kornewald <wkornewald>
2011-03-24 23:01:29 +0100

    simplified settings

--- settings.py
+++ settings.py
@@ -5,6 +5,15 @@ from djangoappengine.settings_base import *
 
 import os
 
+# Uncomment this if you're using the high-replication datastore.
+# TODO: Once App Engine fixes the "s~" prefix mess we can remove this.
+#DATABASES['default']['HIGH_REPLICATION'] = True
+
+# Activate django-dbindexer for the default database
+DATABASES['native'] = DATABASES['default']
+DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
+DBINDEXER_SITECONF = 'dbindexes'
+
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
@@ -13,12 +22,16 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.sessions',
     'djangotoolbox',
+    'dbindexer',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
 )
 
 MIDDLEWARE_CLASSES = (
+    # This loads the index definitions, so it has to come first
+    'dbindexer.middleware.DBIndexerMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -38,15 +51,3 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 
 ROOT_URLCONF = 'urls'
-
-# Activate django-dbindexer if available
-try:
-    import dbindexer
-    DATABASES['native'] = DATABASES['default']
-    DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
-    INSTALLED_APPS += ('dbindexer',)
-    DBINDEXER_SITECONF = 'dbindexes'
-    MIDDLEWARE_CLASSES = ('dbindexer.middleware.DBIndexerMiddleware',) + \
-                         MIDDLEWARE_CLASSES
-except ImportError:
-    pass
