7cb79b98fa0e0f9e3d0ebee15b57be51217c5040
wrwrwr <git@wr.waw.pl>
2012-01-02 13:39:32 +0100

    Enable admin and add its url setup.

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ AUTOLOAD_SITECONF = 'indexes'
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
-#    'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.auth',
     'django.contrib.sessions',
--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+
 
 handler500 = 'djangotoolbox.errorviews.server_error'
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
+
+    url(r'^admin/', include(admin.site.urls)),
 )
