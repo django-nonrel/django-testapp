0c1245b381ab6a27c8ec3ff68f16ed84969b539f
Marco Seguri <marco.seguri@gmail.com>
2013-09-24 17:38:46 +0200

    django admin enabled by default

--- urls.py
+++ urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+import dbindexer
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
+# django admin
+admin.autodiscover()
+
+# search for dbindexes.py in all INSTALLED_APPS and load them
+dbindexer.autodiscover()
+
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    ('^$', 'django.views.generic.simple.direct_to_template',
-     {'template': 'home.html'}),
+    ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    ('^admin/', include(admin.site.urls)),
 )
