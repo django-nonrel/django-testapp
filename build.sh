21496aaf6879c816a27c8bbde245ec4b5c006fcb
Marco Seguri <marco.seguri@gmail.com>
2013-09-24 17:34:02 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-mkdir build
+DOWNLOAD_DIR=./src
 
-pip install --download='./build' --no-install -r requirements.txt
+# test for git and hg commands
+for cmd in git hg; do
+    command -v ${cmd} >/dev/null || { echo "sh: command not found: ${cmd}"; exit 1; }
+done
 
-unzip -q build/django-autoload-0.01.zip -d build
-unzip -q build/django-dbindexer-0.3.zip -d build
-unzip -q build/django-nonrel-1.4.5.zip -d build
-unzip -q build/djangoappengine-1.0.zip -d build
-unzip -q build/djangotoolbox-0.9.2.zip -d build
+# download all packages to ./src (default)
+pip install --no-install -r requirements.txt
 
-cp -r build/django-autoload/autoload ./autoload
-cp -r build/django-dbindexer/dbindexer ./dbindexer
-cp -r build/django-nonrel/django ./django
-cp -r build/djangoappengine/djangoappengine ./djangoappengine
-cp -r build/djangotoolbox/djangotoolbox ./djangotoolbox
+cp -r ${DOWNLOAD_DIR}/django-autoload/autoload ./autoload
+cp -r ${DOWNLOAD_DIR}/django-dbindexer/dbindexer ./dbindexer
+cp -r ${DOWNLOAD_DIR}/django-nonrel/django ./django
+cp -r ${DOWNLOAD_DIR}/djangoappengine/djangoappengine ./djangoappengine
+cp -r ${DOWNLOAD_DIR}/djangotoolbox/djangotoolbox ./djangotoolbox
 
-rm -r ./djangoappengine/djangoappengine.egg-info
+[ -f ${DOWNLOAD_DIR}/pip-delete-this-directory.txt ] && rm -rf ${DOWNLOAD_DIR}
+
+echo "Now run:
+./manage.py runserver
+
+To launch this app. If you want access to django /admin, run also:
+./manage.py createsuperuser
+
+And then login in <your_app_ip>/admin, probably http://127.0.0.1:8000/admin"
+
+
